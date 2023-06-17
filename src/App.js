fe4d12cdcf893d0b5a0daa520cbcd1eede88242e
brightkams <ohakambright@gmail.com>
2023-06-17 23:08:44 +0100

    commit fpr tailwind configuration and styles

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
-import './App.css';
-import Employee from './components/Employee';
-import {useState} from "react";
-
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
 
 function App() {
-  let newRole = "NO ROLE"
-  const[role, setRole] = useState(newRole);
+  let newRole = " No Role yet";
+  let newMessage = "message";
+  const [role, setRole] = useState(newRole);
+  const [employees, setEmployees] = useState(
+    [
+      {name: "Bright"}
+    ]
+  );
+  const [message, setMessage] = useState(newMessage);
 
   // let role = "dev"
   const showEmployees = true;
   return (
-    <div className="App">
-     {showEmployees ? 
-      <> 
-      <input type="text" onChange={(e)=> {
-        console.log(e.target.value);
-        setRole(e.target.value);
-      }} 
-      />
-      <Employee name="Bright" role="Intern" />
-      <Employee name="Ohakam" role={role}/>
-      <Employee name="Chidozie" role="Cyber Security"/>
-      {/* <Employee />
-      <Employee /> */}
-      
-      </>
-      :
-      
-      (<p>You cannot see the employees.</p>)
-      
-    }
-      
+    <div className="App bg-red-400 py-0.5">
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+            className="text-center block my-3 w-96 px-5 py-3 mx-10"
+          />
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setMessage(e.target.value);
+            }}
+            className="text-center block my-3 w-96 px-5 py-3 mx-10"
+          />
+          <div className="flex flex-wrap justify-center space-2">
+            <Employee
+              name="Bright"
+              role="Intern"
+              message={message}
+              image="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+            />
+            <Employee
+              name="Ohakam"
+              role={role}
+              message={message}
+              image="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+            />
+            <Employee
+              name="Chidozie"
+              role="Cyber Security"
+              message={message}
+              image="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+            />
+            <Employee
+              name="Bright"
+              role={role}
+              message={message}
+              image="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+            />
+            <Employee
+              name="No Name yet"
+              role={role}
+              message={message}
+              image="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+            />
+          </div>
+        </>
+      ) : (
+        <p>You cannot see the employees.</p>
+      )}
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,29 @@
 function Employee(props){
-    return  ( 
-    <> 
-        <h3>Employee {props.name}</h3>
-        <h2> Hello, {props.name} how are You!</h2>
-        {/* <p>{props.role ? props.role : "No role"}</p> */}
-        {/* OR */} 
-        {props.role ? <p class="role">{props.role}</p> : <p class="norole">No role</p>}
-    </>
+    return      ( 
+
+
+        <div class="py-8 m-10 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+  <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0 border" src={props.image} alt="User's avatar" />
+  <div class="text-center space-y-2 sm:text-left">
+    <div class="space-y-0.5">
+      <p class="text-lg text-black font-semibold border max-w  my-5 px-2 text-center rounded-full hover:bg-blue-500  hover:text-white">
+        {props.name}
+      </p>
+      <p class="text-slate-500 font-medium w-48 border py-1  px-3 text-center rounded-full hover:bg-blue-500  hover:text-white"  >
+        {props.role}
+      </p>
+    </div>
+    <button class="px-4 py-1.5 text-sm text-purple-600 font-semibold rounded-full border  border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 text-center ">{props.message}
+    </button>
+  </div>
+</div>
+    // <> 
+    //     <h3>Employee {props.name}</h3>
+    //     <h2> Hello, {props.name} how are You!</h2>
+    //     {/* <p>{props.role ? props.role : "No role"}</p> */}
+    //     {/* OR */} 
+    //     {props.role ? <p class="role">{props.role}</p> : <p class="norole">No role</p>}
+    // </>
     );
     
 }
